29335818516c08ea6060b54132916d1b29d44727
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-15 15:56:43 -0300

    Create math.py

---
+++ math.py
@@ -0,0 +1,26 @@
+#Operações Matematicas
+3 + 5
+7 - 4
+3 * 2
+print(6 / 3)
+
+# Divisão retorna variavel Float
+
+print(type(6/3))
+# 2.0
+# <class 'float'>
+
+# Expoente
+print(2 ** 3)
+# 8
+
+# PEMDAS (ORDEM DE PRIORIDADE)
+# Parentheses ()
+# Exponents **
+# Multiplication *
+# Division /
+# Addition +
+# Subtraction -
+
+print(3 * 3 + 3 / 3 - 3)
+# 7.0
