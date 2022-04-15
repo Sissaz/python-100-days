18b2dba19de033e369567af53d8ab88507e975dd
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-15 15:17:53 -0300

    complete

---
+++ day01/replit-exercises.py
@@ -0,0 +1,43 @@
+# 1º Exercicio
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# 2º Exercicio
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# 3º Exercicio
+print( 
+len(
+input("What is your name? ") ) )
+####
+
+# 4º Exercicio
+print("The program below switches the values ​​stored in the variables a and b.")
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b 
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
