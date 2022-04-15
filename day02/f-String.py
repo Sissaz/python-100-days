138b107ca7cc73401eab6f86d815f771711b4362
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-15 16:59:19 -0300

    complete

--- day02/f-String.py
+++ day02/f-String.py
@@ -5,3 +5,7 @@ height = 1.8
 isWinning = True
 
 print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+# your score is 0, your height is 1.8, you are winning is True
+
+# Variaveis de diferentes tipos no mesmo print
