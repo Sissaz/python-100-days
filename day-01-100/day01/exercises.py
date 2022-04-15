37475289de982aec4fa6d1e2fef14310df1dffdb
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-15 18:43:24 -0300

    complete

---
+++ day-01-100/day01/exercises.py
@@ -0,0 +1,51 @@
+# 1° Exercise
+# Print!
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+ 
+  
+# 2° Exercise
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+# 3° Exercise
+
+#Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+print( 
+len(
+input("What is your name? ") ) )
+
+
+# 4° Exercise
+
+#Write a program that switches the values stored in the variables a and b.
+
+print("The program below switches the values ​​stored in the variables a and b.")
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+c = a
+a = b 
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
