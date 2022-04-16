fb0eef3e7088b32004e2b39ac143c38b5a7b7a71
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-16 19:36:08 -0300

    Create if-else.py

---
+++ day-01-100/day03/if-else.py
@@ -0,0 +1,35 @@
+#If and Else:
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height > 120: 
+  print("You can ride the rollercoaster!")
+else:
+  ("Sorry, you have to grow taller before you can ride.")
+  
+#Cuidado com as indentações do if e do else!
+
+Welcome to the rollercoaster!
+What is your height in cm? 163
+You can ride the rollercoaster!
+
+#Para incluir o 120:
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120: 
+  print("You can ride the rollercoaster!")
+else:
+  ("Sorry, you have to grow taller before you can ride.")
+
+# > Greater than
+# < Less than
+# >= Greater than or equal to
+# <= Less than or equal to
+# == Equal to
+# != Not equal to
+
+# = Assignment (Variaveis)
+# == Check Equality (if height == 120)
