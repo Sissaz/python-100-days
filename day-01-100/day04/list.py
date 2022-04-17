b3b5b8b4dc9f29c39abb3fc799f11eb1d11c4fcd
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-17 17:20:59 -0300

    complete

---
+++ day-01-100/day04/list.py
@@ -0,0 +1,29 @@
+# Lists
+
+# OBS: [] Might be related to a list!
+
+
+states_of_america = ["Delaware", "Pennsylvania"]
+print(states_of_america[0])
+# Delaware
+
+
+states_of_america = ["Delaware", "Pennsylvania"]
+print(states_of_america)
+
+# ['Delaware', 'Pennsylvania']
+
+
+
+# Add an item to the end of the list:
+
+states_of_america.append("Sicilia Island")
+print(states_of_america)
+
+#['Delaware', 'Pennsylvania', 'Sicilia Island']
+
+
+
+
+
+
