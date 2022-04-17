8c78b7272600df9d3f9940f3d5c11b8fb787c4be
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-17 18:12:47 -0300

    Create nested-lists.py

---
+++ day-01-100/day04/nested-lists.py
@@ -0,0 +1,20 @@
+# Lists
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
+
+##
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
+
+# Primeiro [1] diz respeito a qual lista irá printar
+# Segundo [1] diz respeito da posição do item que irá printar
