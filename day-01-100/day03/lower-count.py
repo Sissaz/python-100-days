35f7e55ddddbcb5c50552e2ac67db98cebaff56f
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-16 23:14:05 -0300

    complete

---
+++ day-01-100/day03/lower-count.py
@@ -0,0 +1,10 @@
+#Lower and Count
+
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+name_lower = name1.lower()
+print(f"{name_lower}")
+
+name_count = name_lower.count("s")
+print(f"{name_count}")
