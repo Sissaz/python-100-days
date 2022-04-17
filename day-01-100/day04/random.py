3ac0657c7e2eb7f6ac248b3f427aace4dd9eef72
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-17 16:54:43 -0300

    complete

--- day-01-100/day04/random.py
+++ day-01-100/day04/random.py
@@ -4,3 +4,21 @@ import random
 random_integer = random.randint(1,10)
 print(random_integer)
 
+love_score = random.randint (1, 100)
+print(f"Your love score is {love_score}")
+
+# Importing Module
+
+import my_module
+print(my_module.pi)
+
+
+# Random between 0 and 1
+
+random_float = random.random()
+print(random_float)
+
+# Expandindo de 0 até 5
+
+random_float * 5
+
