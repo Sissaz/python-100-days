9d5fddcd693703b5c543a453697420429a3a0edd
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-18 23:31:36 -0300

    complete

---
+++ day-01-100/day05/for-range.py
@@ -0,0 +1,33 @@
+# For Loop with Range
+
+for number in range(1,11,1):
+  print(number)
+  
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+#
+
+for number in range(1,11,4):
+  print(number)
+  
+# 1
+# 5
+# 9 
+
+#
+
+total = 0
+for number in range(1,101):
+  total += number
+print(total)
+
+#5050
