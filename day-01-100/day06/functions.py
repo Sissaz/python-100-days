71996ad41781837da2b5a210c61eaa065b2f3f62
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-21 23:36:14 -0300

    Create functions.py

---
+++ day-01-100/day06/functions.py
@@ -0,0 +1,35 @@
+# Functions
+
+https://docs.python.org/3/library/functions.html
+  
+# Sabemos que é uma função por estar seguida do nome da função e parenteses
+
+# Making our own functions
+# 1° Start with a key word:
+def
+# Because we are creating or defining a function
+
+# 2° Giving the function a name:
+def my_function
+
+# 3° Para indicar que é uma função, não esquecer os parenteses:
+def my_function()
+
+# 4° Não esquecer os dois pontos:
+def my_function():
+  
+# 5° Calling Functions: Não esquecer de summonar a função quando precisar (se não, nada irá acontecer):
+# Não esquecer de indentar o que a função fará.
+
+def siciliafunction():
+  print("Hello")
+  print("Bye")
+
+➡️ siciliafunction()
+
+
+
+
+
+
+
