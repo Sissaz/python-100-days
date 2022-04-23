91fd9b46a78d8565b261e9b2e2d16567f46b7824
Sicilia Giacomazza <102394519+Sissaz@users.noreply.github.com>
2022-04-23 19:36:08 -0300

    complete

---
+++ day-01-100/day06/reeborg-while.py
@@ -0,0 +1,32 @@
+# For and While
+
+for item in list_of_items:
+	#Do something to each item
+
+for number in range(a,b):
+	print(number)
+  
+while something_is_true:
+  #Do something repeatedly
+
+# Reeborg World Exercise
+# Chasing the Flag!
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+def virar():
+    turn_left()
+    turn_left()
+    turn_left()
+def caminho():
+    move()
+    turn_left()
+    move()
+    virar()
+    move()
+    virar()
+    move()
+    turn_left()
+while not at_goal():
+    caminho()
+    if at_goal()is True:
+        break
